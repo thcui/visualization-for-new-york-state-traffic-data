8792bff04a3be50ad2840390bb1a2659c305eff4
thcui <cthtyty@163.com>
2021-12-07 04:18:17 -0500

    added the draft interactive plot

--- 06-interactive/main.js
+++ 06-interactive/main.js
@@ -2,11 +2,23 @@
 
 
 let data=[]
-let selectedDomain = [new Date('2018-01-01'), new Date('2018-12-31')];
-const parseTime = d3.timeParse("%Y-%m-%d");
+let selectedDomain = [new Date('2018-01'), new Date('2018-12')];
+const parseTime = d3.timeParse("%Y-%m");
 
 d3.csv('../data/data_for_interactivity.csv').then(_d=>{
     data= _d
-    data[Date]=parseTime(data[Date]);
+
+    data.forEach(row => {
+        Object.keys(row).forEach(attr => {
+            if (attr === 'date') {
+                row[attr] = parseTime(row[attr]);
+            }
+
+            if (attr === 'count') {
+                row[attr] = +row[attr];
+            }
+        })
+    });
+    delete data.columns
     let lineChart = new LineChart('#lineChart', data)
 })
